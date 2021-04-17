55d532063810b0ccff86d9ff8e4544faa2cd4f15
Julian Zhou <julianqz@users.noreply.github.com>
2021-04-17 13:28:13 -0700

    base image and aux R script

---
+++ wu_base/wu_base_r.R
@@ -0,0 +1,32 @@
+ARGS = commandArgs(trailingOnly=T)
+stopifnot(length(ARGS)==1)
+
+# order of command line arguments should match this
+PACKAGES = c("alakazam")
+
+REPO_CRAN = "http://cran.us.r-project.org"
+#REPO_CRAN = "http://lib.stat.cmu.edu/R/CRAN/"
+
+# need this to install from within Docker
+options(repos=REPO_CRAN)
+
+VERSIONS_WANTED = ARGS
+names(VERSIONS_WANTED) = PACKAGES
+
+# for getting versions of installed packages
+install.packages("versions")
+
+# Immcantation dependencies in CRAN
+# only optparse and BiocManager not in jupyter/datascience-notebook
+# c("Rcpp", "devtools", "roxygen2", "testthat", "pkgbuild", "rmarkdown","knitr", "optparse", "BiocManager")
+install.packages(c("optparse", "BiocManager"))
+
+# Alakazam dependencies in BioConductor
+BC_DEP = c("Biostrings", "GenomicAlignments", "IRanges")
+BiocManager::install(BC_DEP)
+
+for (pkg in PACKAGES) {
+
+    devtools::install_version(pkg, version=VERSIONS_WANTED[pkg])
+
+}
