ca0764002f137f4e99633c676f91638d6e0fb62e
Julian Zhou <julianqz@users.noreply.github.com>
2021-04-17 13:28:37 -0700

    presto image and aux r script

---
+++ wu_presto/wu_presto_r.R
@@ -0,0 +1,16 @@
+# https://bitbucket.org/kleinstein/prestor/src/master/README.md
+
+REPO_CRAN = "http://cran.us.r-project.org"
+#REPO_CRAN = "http://lib.stat.cmu.edu/R/CRAN/"
+
+# need this to install from within Docker
+options(repos=REPO_CRAN)
+
+# first install dependencies
+#* already in jupyter/datascience-notebook
+#install.packages(c("devtools", "roxygen2"))
+
+# install the latest development code via devtools
+#* "javh/prototype-prestor@default" doesn't work any more -- repo has changed location
+library(devtools)
+install_bitbucket("kleinstein/prestor@master")
