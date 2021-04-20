239ca53bb9de6ce21de83fccd1467d66f257b24d
Julian Zhou <julianqz@users.noreply.github.com>
2021-04-19 19:57:57 -0700

    Updated variable names

--- wu_presto/scripts/remove_inconsistent_c.py
+++ wu_presto/scripts/remove_inconsistent_c.py
@@ -11,17 +11,17 @@ from sys import argv
 # header: 
 # @TAGTATTAATCTACGCA|CONSCOUNT=1,1|PRCONS=Human-IGHG|SEQORIENT=F|CREGION=Human-IGHG-InternalC
 
-HEADER_MID_FW = "PRCONS"
-HEADER_MID_RV = "CREGION"
+HEADER_1 = "PRCONS"  #*
+HEADER_2 = "CREGION" #*
 
-PATTERN_MID_FW = re.compile(HEADER_MID_FW+"=[\w_-]+\|?")
-PATTERN_MID_RV = re.compile(HEADER_MID_RV+"=[\w_-]+\|?")
+PATTERN_1 = re.compile(HEADER_1+"=[\w_-]+\|?")
+PATTERN_2 = re.compile(HEADER_2+"=[\w_-]+\|?")
 
 # function to separate, if any, inconsistent pairs from consistent ones
 # outputs 3 lists:
 # - consistentLines: fastq lines corresponding to sequences with consistent pairs
 # - inconsistentLines: fastq lines corresponding to sequences with inconsistent pairs
-# - inconsistentRecords: "MID_FW, MID_RV" corresponding to sequences with inconsistent pairs
+# - inconsistentRecords: "${HEADER_1}, ${HEADER_2}" corresponding to sequences with inconsistent pairs
 def spotInconsistency(inputFasta):
     
     with open(inputFasta, "r") as in_handle:
@@ -41,31 +41,31 @@ def spotInconsistency(inputFasta):
             # header
             curHeader = lines[idx]
             
-            # parse for MID_FW
-            MIDfwMatch = PATTERN_MID_FW.findall(curHeader)
+            # parse for ${HEADER_1}
+            match_1 = PATTERN_1.findall(curHeader)
 
-            if len(MIDfwMatch)==1:
-                # remove MID_FW=
-                MIDfw = re.sub(HEADER_MID_FW+"=", "", MIDfwMatch[0])
+            if len(match_1)==1:
+                # remove ${HEADER_1}=
+                value_1 = re.sub(HEADER_1+"=", "", match_1[0])
                 # if there's |, remove it
-                MIDfw = re.sub("\|", "", MIDfw)
-            elif len(MIDfwMatch)==0:
-                sys.exit("No match to PATTERN_MID_FW found in header")
+                value_1 = re.sub("\|", "", value_1)
+            elif len(match_1)==0:
+                sys.exit("No match to "+HEADER_1+" found in header")
             else:
-                sys.exit("More than exactly one matches to PATTERN_MID_FW found in header")
+                sys.exit("More than exactly one matches to "+HEADER_1+" found in header")
             
-            # parse for MID_RV
-            MIDrvMatch = PATTERN_MID_RV.findall(curHeader)
+            # parse for ${HEADER_2}
+            match_2 = PATTERN_2.findall(curHeader)
 
-            if len(MIDrvMatch)==1:
-                # remove MID_RV=
-                MIDrv = re.sub(HEADER_MID_RV+"=", "", MIDrvMatch[0])
+            if len(match_2)==1:
+                # remove ${HEADER_2}=
+                value_2 = re.sub(HEADER_2+"=", "", match_2[0])
                 # if there's |, remove it
-                MIDrv = re.sub("\|", "", MIDrv)
-            elif len(MIDrvMatch)==0:
-                sys.exit("No match to PATTERN_MID_RV found in header")
+                value_2 = re.sub("\|", "", value_2)
+            elif len(match_2)==0:
+                sys.exit("No match to "+HEADER_2+" found in header")
             else:
-                sys.exit("More than exactly one matches to PATTERN_MID_RV found in header")
+                sys.exit("More than exactly one matches to "+HEADER_2+" found in header")
 
             # if not the last header
             # copy over lines containing current sequence
@@ -76,15 +76,16 @@ def spotInconsistency(inputFasta):
             # copy over remaining lines containing sequence
                 nextIdx = len(lines)
 
-            # if matched 
-            if MIDfw in MIDrv:
+            # if matched
+            #* assumes PRCONS being a substring of CREGION if consistent
+            if value_1 in value_2:
                 consistentLines.append(curHeader)
 
                 for seqIdx in range(idx+1, nextIdx):
                     consistentLines.append(lines[seqIdx])
             else:
                 inconsistentLines.append(curHeader)
-                inconsistentRecords.append(MIDfw+", "+MIDrv)
+                inconsistentRecords.append(value_1+", "+value_2)
 
                 for seqIdx in range(idx+1, nextIdx):
                     inconsistentLines.append(lines[seqIdx])                
@@ -109,9 +110,9 @@ def outputLines(consistentLines, inconsistentLines, inconsistentRecords, outputN
         with open(outputNameConsistent, 'w') as out_handle:
             out_handle.writelines(line+"\n" for line in consistentLines)
 
-        print("> Output for sequences with consistent MID_FW and MID_RV: "+outputNameConsistent+"\n")
+        print("> Output for sequences with consistent "+HEADER_1+" and "+HEADER_2+": "+outputNameConsistent+"\n")
     else:
-        print("> No sequence with consistent MID_FW and MID_RV found.\n")
+        print("> No sequence with consistent "+HEADER_1+" and "+HEADER_2+" found.\n")
 
     if numInconsistentLines>=1:
         outputNameInconsistent = outputNameStem+"_inconsistent.fastq"
@@ -119,21 +120,21 @@ def outputLines(consistentLines, inconsistentLines, inconsistentRecords, outputN
         with open(outputNameInconsistent, 'w') as out_handle:
             out_handle.writelines(line+"\n" for line in inconsistentLines)
 
-        print("> Output for sequences with inconsistent MID_FW and MID_RV: "+outputNameInconsistent+"\n")
+        print("> Output for sequences with inconsistent "+HEADER_1+" and "+HEADER_2+": "+outputNameInconsistent+"\n")
     else:
-        print("> No sequence with inconsistent MID_FW and MID_RV found.\n")
+        print("> No sequence with inconsistent "+HEADER_1+" and "+HEADER_2+" found.\n")
         
     if len(inconsistentRecords)>=1:
         outputNameRecord = outputNameStem+"_inconsistent_count.txt"
         
-        # tabulate counts for MID_FW-MID_RV pairs
+        # tabulate counts for ${HEADER_1}-${HEADER_2} pairs
         recordCount = [item+", {0}".format(inconsistentRecords.count(item)) for item in set(inconsistentRecords)]
         
         with open(outputNameRecord, 'w') as out_handle:
-            out_handle.writelines("MID_FW, MID_RV, COUNT\n")
+            out_handle.writelines(HEADER_1+", "+HEADER_2+", COUNT\n")
             out_handle.writelines(line+"\n" for line in recordCount)
 
-        print("> Output for counts of pairs of inconsistent MID_FW and MID_RV: "+outputNameRecord+"\n")
+        print("> Output for counts of pairs of inconsistent "+HEADER_1+" and "+HEADER_2+": "+outputNameRecord+"\n")
 
     print("# consistent seqs: {0} ; # inconsistent seqs: {1}.\n".format(numConsistentLines, numInconsistentLines))
 
