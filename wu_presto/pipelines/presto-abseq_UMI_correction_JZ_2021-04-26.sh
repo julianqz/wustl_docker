5693b73e059921da7907dada5d80cd7065e4f1fa
Julian Zhou <julianqz@users.noreply.github.com>
2021-04-27 03:22:09 -0700

    fixed STEP in post-indexing; remove -n 2 for debugging in EE

--- wu_presto/pipelines/presto-abseq_UMI_correction_JZ_2021-04-26.sh
+++ wu_presto/pipelines/presto-abseq_UMI_correction_JZ_2021-04-26.sh
@@ -530,7 +530,6 @@ if $BOOL_MID; then
     EstimateError.py set \
         -s "${OUTNAME}-uid_cluster-pass.fastq" \
         -f INDEX_UID \
-        -n 2 \
         --nproc "${NPROC}" \
         --outname "${OUTNAME}" --outdir . --log "${LOGDIR}/EstimateErrorSet.log" \
         >> $PIPELINE_LOG 2> $ERROR_LOG
@@ -669,7 +668,8 @@ if $BOOL_POST; then
         echo -e "DIRECTORY: ${OUTDIR}"
         echo -e "PRESTO VERSION: ${PRESTO_VERSION}"
         echo -e "\nSTART"
-        STEP=$(($STEP_INIT+1))
+        #STEP=$(($STEP_INIT+1))
+        STEP="${STEP_INIT}"
 
 
         # 
