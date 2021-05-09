3e85a29965900cdde4b57363f3af08c519a16eca
Julian Zhou <julianqz@users.noreply.github.com>
2021-05-09 01:55:06 -0700

    added installation of other R pkgs
    E.g. fastcluster, corrplot

---
+++ wu_cimm/wu_cimm_other.R
@@ -0,0 +1,13 @@
+PACKAGES = commandArgs(trailingOnly=T)
+
+REPO_CRAN = "http://cran.us.r-project.org"
+#REPO_CRAN = "http://lib.stat.cmu.edu/R/CRAN/"
+
+# need this to install from within Docker
+options(repos=REPO_CRAN)
+
+for (pkg in PACKAGES) {
+    
+    install.packages(pkg)
+
+}
