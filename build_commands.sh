f204298bb9ecc6f37253b76714a286c759654477
Julian Zhou <julianqz@users.noreply.github.com>
2021-12-22 14:42:31 -0500

    improved and updated release notes

--- build_commands.sh
+++ build_commands.sh
@@ -3,16 +3,64 @@
 
 ### versions
 
+# base   0.1.0  Ubuntu 20.04.2 LTS (includes g++ 9.3.0)
+#               Python 3.8.8
+#               R 4.0.3
+#               alakazam 1.1.0
+#               knitr 1.31
+#               BiocManager 1.30.12 (Bioconductor 3.12)
+#
+#         0.2.0 Ubuntu 20.04.3 LTS (no g++)
+#               Python 3.9.7
+#               R 4.1.1
+#               alakazam 1.2.0
+#               knitr 1.32
+#               BiocManager 1.30.16 (Bioconductor 3.14)
+
 # presto 0.1.0  initial
-#        0.1.1  increase cd-hit-est limit
+#               pRESTO 0.6.2
+#        0.1.1  increase cd-hit-est memory limit
 #        0.1.1d debugging for EE set (no fix) 
+#
+#        0.2.0  pRESTO 0.7.0 (includes parameter to increase cd-hit-est memory limit)
+#               prestor fixed compatbility issue with knitr 1.32
 
 # cimm   0.1.0  initial
+#               changeo 1.0.2
+#               scanpy 1.7.2
+#               Seurat 4.0.1
+#               PHYLIP 3.697
+#               IgPHYML 1.1.3
+#               shazam 1.0.2
+#               tigger 1.0.0
+#               scoper 1.1.0
+#               tbl2asn 25.8
+#               
 #        0.1.1  bug fixes for shazam::distToNearest (v1.0.2) and 
 #                             tigger::genotypeFasta (v1.0.0)
-#               scikit-misc
-#               add C57BL6 germline
-#               LSF env variable for scanpy to work on RIS (ref_0.1.1_lsf)
+#               scikit-misc 0.1.4 (new addition)
+#
+#
+#        0.2.0  changeo 1.2.0
+#               scanpy 1.8.2
+#               Seurat 4.0.6
+#               scRepertoire 1.4.0 via Bioconductor 3.14 (new addition)
+#               shazam 1.1.0
+#               scoper 1.2.0
+#               dowser 0.1.0 via CRAN (new addition)
+#               tbl2asn - (removed)
+
+# ref   0.1.x   igblastn 1.17.1
+#       0.1.x   IMGT references (standard): release202113-2
+#               IMGT references (C57BL6): -
+#       0.1.1   IMGT references (standard): release202113-2
+#               IMGT references (C57BL6): release202011-3
+#       
+#       0.2.x   igblastn 1.18.0 (C gene support for Ig seqs)
+#               IMGT references (standard): release202150-3
+#               IMGT references (C57BL6): release202011-3
+
+# ref_lsf       added LSF env variable for scanpy to work on RIS
 
 
 
@@ -20,7 +68,7 @@
 
 # jupyter/datascience-notebook
 #	
-# 	 wu_base:main_: bioconductor dependencies, alakazam
+# 	 wu_base:main_: Bioconductor dependencies, alakazam, knitr
 #
 #		     wu_presto:main_: presto, prestor (alakazam-dependent), muscle, vsearch, CD-HIT, BLAST
 #	                          abseq pipeline
@@ -30,9 +78,9 @@
 #	        			     _lsf: ENTRYPOINT
 #
 #	         wu_cimm:main_: changeo, shazam, tigger, scoper
-#		                    scanpy, seurat
+#		                    scanpy, Seurat, (scRepertoire)
 #		                    PHYLIP, igphyml, (dowser)
-#	        	            tbl2asn
+#	        	            (tbl2asn)
 #
 #		            wu_cimm:ref_: IgBLAST, IMGT
 #
@@ -53,15 +101,21 @@ cd "/Users/jqz/Dropbox/wustl/code/docker/"
 
 docker build --file wu_base/wu_base_dockerfile --tag julianqz/wu_base:main_0.1.0 ./wu_base
 
+docker push julianqz/wu_base:main_0.1.0
+
 docker build --file wu_presto/wu_presto_dockerfile --tag julianqz/wu_presto:main_0.1.1 \
 	--build-arg BASE_CONTAINER="wu_base:main_0.1.0" ./wu_presto
 
-docker build --file wu_cimm/wu_cimm_dockerfile --tag julianqz/wu_cimm:main_0.1.1 \
+docker push julianqz/wu_presto:main_0.1.1
+
+docker build --file wu_cimm/wu_cimm_dockerfile_v0.1 --tag julianqz/wu_cimm:main_0.1.1 \
 	--build-arg BASE_CONTAINER="wu_base:main_0.1.0" ./wu_cimm
+
+docker push julianqz/wu_cimm:main_0.1.1
 
 # imgt refs
 
-./wu_ref/wu_ref_get_imgt.sh \
+./wu_ref/wu_ref_imgt_get.sh \
 	-a "/Users/jqz/Dropbox/common/germline_refs/imgt_zip/" \
 	-b "/Users/jqz/Dropbox/common/germline_refs/imgt_select/" \
 	-c "/Users/jqz/Dropbox/common/germline_refs/imgt_download.log" \
@@ -75,9 +129,13 @@ docker build --file "/Users/jqz/Dropbox/wustl/code/docker/wu_ref/wu_ref_dockerfi
 	--tag julianqz/wu_presto:ref_0.1.1 --build-arg BASE_CONTAINER="wu_presto:main_0.1.1" \
 	"/Users/jqz/Dropbox/"
 
+docker push julianqz/wu_presto:ref_0.1.1
+
 docker build --file "/Users/jqz/Dropbox/wustl/code/docker/wu_ref/wu_ref_dockerfile" \
 	--tag julianqz/wu_cimm:ref_0.1.1 --build-arg BASE_CONTAINER="wu_cimm:main_0.1.1" \
 	"/Users/jqz/Dropbox/"
+
+docker push julianqz/wu_cimm:ref_0.1.1
 
 # add lsf
 
